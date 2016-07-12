e9387e5bcdfee27fc5563f605a3395e720c09b5b
yokomizo <yokomizo@fiilse.com>
2016-07-12 10:46:41 +0000

    kaminari

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get    'login' , to: 'sessions#new'
   post   'login' , to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
+  #get    'pagenate', to: 'static_pages#index'
 
   resources :users
   resources :users do

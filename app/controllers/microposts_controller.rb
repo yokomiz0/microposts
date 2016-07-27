469d45bb14682db43a44df268a15f0b242792570
yokomizo <yokomizo@fiilse.com>
2016-07-27 11:49:48 +0000

    microposts-10

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -7,7 +7,7 @@ class MicropostsController < ApplicationController
       flash[:success] = "Micropost created!"
       redirect_to root_url
     else
-      @feed_items = current_user.feed_items.includes(:user).order(created_at: :desc)
+      @feed_items = current_user.feed_items.includes(:user).order(created_at: :desc).page(params[:page]).per(5)
       render 'static_pages/home'
     end
   end
